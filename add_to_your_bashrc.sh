a99ce3bac88390dcfbf2c81ea903bb36736d3dc2
Ryan-M3 <gn341ram@gmail.com>
2016-04-08 15:41:32 -0700

    oops.

--- add_to_your_bashrc.sh
+++ add_to_your_bashrc.sh
@@ -1,17 +1,17 @@
 function cheat() {
-    WHERE = ~/.cheat
+    WHERE="~/.cheat"
     if [ $1 == 'edit' ]; then
-        vim WHERE/$2
+        vim ${WHERE}/$2
 
-    elif [ $1 == 'make' ]:
-        touch WHERE/$2
+    elif [ $1 == 'make' ]; then
+        touch ${WHERE}/$2
 
-    elif [ $1 == 'add' ]:
-        $3 >> WHERE/$2
+    elif [ $1 == 'add' ]; then
+        $3 >> ${WHERE}/$2
 
     else
         echo ''
-        sed 's/^/      /' WHERE/$1
+        sed 's/^/      /' ${WHERE}/$1
         echo ''
 
     fi
